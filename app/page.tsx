4d0e4795619a6d51e867ae1f6f8bd9878aed39e2
hamzaadil56 <hamzaadil56@gmail.com>
2023-04-14 21:39:51 +0500

    adding-portfolio-link

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,21 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      <div className="flex md:w-4/5 mx-auto  min-h-screen flex-col items-center justify-center place-content-center md:p-24 p-6">
-        <h1 className="md:text-5xl my-4  text-center leading-relaxed font-bold">
+      <div className="flex gap-6 md:w-4/5 mx-auto  min-h-screen flex-col items-center justify-center place-content-center md:p-24 p-6">
+        <h1 className="md:text-5xl   text-center leading-relaxed font-bold">
           Are you building a DApp that wants to onboard Web2 users? <br />
           Connect Wallets with Social Accounts!
         </h1>
         <ConnectButton />
+        <h1>
+          Made by{" "}
+          <a
+            className="decoration-underline underline decoration-black"
+            href="https://portfolio-hamzaadil56.vercel.app/"
+          >
+            Muhammad Hamza
+          </a>
+        </h1>
       </div>
     </main>
   );
